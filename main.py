d974e3fddddd0fc857d69992fa603932422c23e1
DonAllenII <don.a.alexanderii@gmail.com>
2023-08-23 12:15:03 -0400

    payForParking method

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from random import randint
 #Create the class parking_garage
 class Parking_garage():
     #init attributes
-    def __init__(self,spaces):
+    def __init__(self, spaces, park_price):
         self.tickets = [x for x in range(spaces)]
         self.parkingSpaces = [x for x in range(spaces)]
         self.currentTicket = {}
+        self.price = park_price
 
     def takeTicket(self):  #takeTicket method
         ticket = randint(1,(len(self.parkingSpaces) - 1))
@@ -15,16 +16,17 @@ class Parking_garage():
         #should decrease the amount of parkingSpace avaible by 1
         self.parkingSpaces.remove(ticket)
 
-        self.currentTicket[ticket] = 'unpaid'
-        return
+        self.currentTicket['paid'] = False
+        return ticket
 
-    def payForParking():
+    def payForParking(self):
         #display an input that waits for an amount from the user and store it in a variable
-
+        payment = float(input('Please enter payment..'))
         #if the payment variable is not empty then 
-
+        if payment > self.price:
+            self.currentTicket['paid'] = True
         #update currentTicket dictionary key "paid" to true
-        retun
+        return
     def leaveGarage():
         #if ticket paid, display a message of "Thank You, have a nice day"
         #if ticket not pad, display an input prompt for payment
