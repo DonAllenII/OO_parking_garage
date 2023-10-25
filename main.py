ff9677b4950e78cc0896d94e8db3ee7162f71b8a
DonAllenII <don.a.alexanderii@gmail.com>
2023-10-25 13:08:28 -0400

    updated pay/leaveGarage methods

--- main.py
+++ main.py
@@ -7,52 +7,70 @@ class Parking_garage():
     def __init__(self, spaces, park_price):
         self.tickets = [x for x in range(spaces)]
         self.parkingSpaces = [x for x in range(spaces)]
-        self.currentTicket = {}
+        self.oustandingTickets = {}
+        self.currentTicket = None
         self.price = park_price
 
     def takeTicket(self):  #takeTicket method
-        self.ticket = randint(1,(len(self.parkingSpaces) - 1))
+        self.currentTicket = randint(1,(len(self.parkingSpaces) - 1))
         #should decrease the amount of tickets avaiable by 1
-        self.tickets.remove(self.ticket)
+        self.tickets.remove(self.currentTicket)
         #should decrease the amount of parkingSpace avaible by 1
-        self.parkingSpaces.remove(self.ticket)
+        self.parkingSpaces.remove(self.currentTicket)
 
-        self.currentTicket['paid'] = False
+        self.oustandingTickets[self.currentTicket] = {'paid' : False}
+        print(self.oustandingTickets)
         print("\n\n")
-        print(f'You have space #{self.ticket}.')
+        print(f'You have space #{self.currentTicket}.')
         return
 
     def payForParking(self):
-        #display an input that waits for an amount from the user and store it in a variable
-        payment = float(input(f'Parking cost is {self.price}. Please enter payment..'))
-        #if the payment variable is not empty then 
-        if payment >= self.price:
-            self.currentTicket['paid'] = True
-        #update currentTicket dictionary key "paid" to true
+        while True:
+            try:
+                space = int(input('Please enter space #...'))
+                if space in self.oustandingTickets.keys():
+                #if space a valid oustanding number
+                    self.currentTicket = space
+                    while True:
+                        #display an input that waits for an amount from the user and store it in a variable
+                        payment = float(input(f'Parking cost is {self.price}. Please enter payment..'))
+                        #if the payment variable is not empty then 
+                        if payment >= self.price:
+                            self.oustandingTickets[self.currentTicket]['paid'] = True
+                            #update oustandingTickets dictionary key "paid" to true
+                            break
+                        else:
+                            print("Please pay off ticket in full")
+                    if self.oustandingTickets[self.currentTicket]['paid'] == True:
+                        break
+                else:
+                    print(f'# {space} is not a valid outstanding parking space number')
+            except ValueError:
+                print("Please enter numbers only\n\n")
         return
     def leaveGarage(self):
         while True:
-        #if ticket paid, display a message of "Thank You, have a nice day"
-            if self.currentTicket['paid'] == True:
+        #if currentTicket paid, display a message of "Thank You, have a nice day"
+            if self.oustandingTickets[self.currentTicket]['paid'] ==  True:
                 print('\nThank You, have a nice day!')
             #update parkingSpaces list to increase by 1
-                self.tickets.insert(self.ticket, self.ticket)
+                self.tickets.insert(self.currentTicket, self.currentTicket)
             #update tickets list to increase by 1
-                self.parkingSpaces.insert(self.ticket, self.ticket)
-                del self.currentTicket['paid']
+                self.parkingSpaces.insert(self.currentTicket, self.currentTicket)
+                del self.oustandingTickets[self.currentTicket]
                 break
                 
             clear_output()
 
-        #if ticket not pad, display an input prompt for payment
-            if self.currentTicket['paid'] == False:
+        #if currentTicket not pad, display an input prompt for payment
+            if self.oustandingTickets[self.currentTicket]['paid'] == False:
+                print('Please pay for parking in full before attempting to leave')
                 self.payForParking()
-                print('Please pay off ticket in full')
 
 
     def run(self):
         while True:
-            answer = str(input(" Press 'P' for a parking ticket\n Press 'E' to pay for parking\n Press 'Q' to quit\n\n\n\n"))
+            answer = str(input(" Press 'P' for a parking ticket\n Press 'E' to exit\n Press 'Q' to quit\n\n\n\n"))
             if answer.lower() == 'p':
                 self.takeTicket()
             if answer.lower() == 'e':
